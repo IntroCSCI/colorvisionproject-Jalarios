70852deba5e5a7371912ae5b8d689f8951b4d9f4
Joseph Larios <jalarios@mail.csuchico.edu>
2020-10-10 02:42:27 +0000

    I started working on the project. So I changed the README.md file, main.cpp, and add two additional files.

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 
 ## Description
 
-!!!Replace with a description of what your program does!!!
+My program will ask the user series of questions, which are different colors which are testing for Blue-yellow color blindness. It will keep track of how many colors they can distinguish from one another. This program will will be and can be expanded to ask more than the amount of questions I have written. 
 
 ## Developer
 
-!!!Replace with your name!!!
+Joseph Larios
 
 ## Example
 
@@ -27,20 +27,20 @@ Here is an example of the program running:
 
 ### Variables and Data Types
 
-!!!Replace with a summary and examples of how multiple variables have been used with a variety of data types and why they have been used effectively and appropriately!!!
+I have used only two variable types to start with, which are string and int. I used the string variable to first be able to read out what is in the test files. The second string variable is to check the user's final choice which is asking if it wanted to resart the program. For the int variables I use them to get the user's answers and to count if they got the question correct or wrong. 
 
 ### Input and Output
 
-!!!Replace with a summary and examples of how input and output have been used effectively and appropriately!!!
+To use the input and output effectively I any used cin when I previously asked a question to get a input from the user. For the output I used cout mainly since it is all I need for now, I also used the cout to help with readability and line spacing. 
 
 ### Decisions
 
-!!!Replace with a summary and examples of how multiple decision constructs have been used effectively and appropriately!!!
+For decision I only used the if, else if, and else statement. I used this statement to have it check to see what the user inputs to see if they are right or wrong with there answer. Within these statement it will count the right and wrong answers and display them at the end of the program. They also display a message after each user choice to hint at if they are right or wrong.
 
 ### Iteration
 
-!!!Replace with a summary and examples of how iteration has been used effectively and appropriately!!!
+Iteration in my program is a do-while loop. I have used 3 do-while loops, the first is to loop the whole program if the user wants to resart at the end, the second and third do-while loop is for the if the user inputs a wrong choice that isn't a 1 or 2 for the questions.
 
 ### File Input and Output
 
-!!!Replace with a summary and examples of how input and/or output of files have been used effectively and appropriately!!!
+For the file input and output, I only have 2 test files which I will need to replace later on with actucal color images for the test. For now I only have the program read out what is written in the file. Since I don't know how to use pixel colors yet that is my next step for this program. 
--- main.cpp
+++ main.cpp
@@ -1,9 +1,143 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
 int main()
 {
+  //variables 
+  string finalc, mytext;
+  int firsta=0, correcta=0, wronga=0, seconda=0;
   
+  cout<<"Hello and Welcome to my Blue-yellow color blind test. " <<endl
+      <<"The test will begin easy at the start "
+      <<"and will progressively get harder." <<endl<<endl;
+
+      do
+      {
+        cout<<"============================================="<<endl<<endl;
+          //say that you are about to begin 
+          cout<< "Let's bring up the first two images."<<endl<<endl;
+
+          //open first two images from the first file
+
+          ifstream MyReadFile("Test1.txt");
+  
+    while (getline (MyReadFile, mytext)) 
+    {
+    // Output the text from the file
+    cout << mytext <<endl;
+    }
+
+    cout<<endl;
+
+    //give them an option between the two images (Question 1)
+      do 
+      {
+        cout<<"Which image is blue? (Please selecr 1 or 2)"<<endl;
+
+        //record the response 
+        cin>> firsta;
+
+        //see if the response is correct or wrong
+          if (firsta == 1)
+          {
+          cout<<"Good choice!"<<endl<<endl;
+
+          correcta++;
+          }
+
+          else if (firsta == 2)
+          {
+          cout<<"Nice try!"<<endl<<endl;
+          wronga++;
+          }
+
+          else 
+          {
+            cout<<"Please select 1 or 2."<<endl<<endl;
+          }
+      }
+
+      while ((firsta != 1) && (firsta != 2));
+
+        //spacing for readability
+        cout<<endl;
+        cout<<"============================================="<<endl<<endl<<endl;
+
+      do 
+      {
+        //start question 2
+        cout<<"Here is Question 2."<<endl<<endl;
+
+        //open the nexst two images from the second file
+          ifstream MyReadFile("Test2.txt");
+  
+    while (getline (MyReadFile, mytext)) 
+    {
+    // Output the text from the file
+    cout << mytext <<endl;
+    }
+
+    cout<<endl;
+
+      //give them an option between the two images (Question 2)
+
+        cout<<"Which image is green? (Please select 1 or 2)"<<endl;
+
+        //record the second response 
+        cin>> seconda;
+
+        //see if the response is correct or wrong
+          if (seconda == 1)
+          {
+          cout<<"Nice Try!"<<endl<<endl;
+
+          wronga++;
+          }
+
+          else if (seconda == 2)
+          {
+          cout<<"Good job!"<<endl<<endl;
+          correcta++;
+          }
+
+          else 
+          {
+            cout<<"Please select 1 or 2."<<endl<<endl;
+          }
+      }
+
+      //checks to make sure the user enters a 1 or 2 //repeats if anything else
+      while ((seconda != 1) && (seconda != 2));
+      
+      //spacing for readability 
+      cout<<endl;
+      cout<<"============================================="<<endl<<endl<<endl;
+
+///////////////////////////////////////////////////////////////////////////////////////////
+// END OF PROGRAM, IF EXPANDED DO BEFORE THIS LINE!!///////////////////////////////////////
+
+        //final score
+        cout<<"Your final score is " <<endl
+            <<"                       "<<correcta <<" Correct answers!"<< endl
+            <<"                       "<<wronga << " Incorrect answers!"
+            <<endl;
+
+        //reset the scores
+        correcta=0;
+        wronga=0;
+
+        cout<<endl; //line spacing
+
+      //ask user if they want to resart
+        cout<<"Do you want to restart? (y or n)"<<endl;
+        cin>> finalc;
+        cout<<endl;
+      }
+
+  while (finalc != "n");
+
+  cout<<"Thank you for your particpation!"<<endl;
   return 0;
 }
